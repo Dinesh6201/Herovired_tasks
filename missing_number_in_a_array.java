fb2b82d05af288043f507018db121edab7f995d0
Dinesh Yandrapu <112755198+Dinesh6201@users.noreply.github.com>
2023-02-11 23:23:50 +0530

    Add files via upload

---
+++ missing_number_in_a_array.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+class missing_number_in_a_array
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int a[]=new int[n];
+        for(int i=0;i<n;i++)
+        {
+            a[i]=sc.nextInt();
+        }
+        int dif=a[0]-0;
+        for(int i=0;i<n;i++)
+        {
+            if(dif<(a[i]-i))
+            {
+                while((a[i]-i)!=dif)
+                {
+                    System.out.print((dif+i)+" ");
+                    dif++;
+                } 
+            }           
+        } 
+    }
+}
